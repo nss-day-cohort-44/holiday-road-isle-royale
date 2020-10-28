0879998332e85bfeebeeb97a1acea2d32962b920
Heath Lester <h.allen.lester@gmail.com>
2020-10-28 13:14:09 -0500

    Added attraction data provider for  NSS API

---
+++ scripts/attractions/AttractionProvider.js
@@ -0,0 +1,12 @@
+
+let attractionArray = []
+
+export const useAttractions = () => {
+    return attractionArray.slice()
+}
+
+export const getAttractions = () => {
+    return fetch(`http://holidayroad.nss.team/bizarreries`)
+        .then ( response => response.json () )
+        .then ( parsedAttractions => attractionArray = parsedAttractions)
+}
