1f44d0e404df7588af84d991c8d16291038f7dc4
Heath Lester <h.allen.lester@gmail.com>
2020-10-29 15:24:00 -0500

    Fixed most data, date still broken

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -17,7 +17,7 @@ eventHub.addEventListener("parkSelect", changeEvent => {
             return selectedPark.id === changeEvent.detail.parkThatWasChosen
         })
         console.log(foundPark)
-        debugger
+        // debugger
         getWeather(foundPark)
             .then(() => {
                 let fiveDayForecast = ``
@@ -40,13 +40,13 @@ const render = (weatherObj) => {
 
 const forecastHTML = (api) => {
     return `
-    <div id="${api.weather.id}" class="weatherCard">
+    <div id="${api.weather[0].id}" class="weatherCard">
         <h3 class="weatherCard__date">${new Date(api.dt).toLocaleDateString('en-US')}</h3>
-        <img class="weatherCard__icon" src="http://openweathermap.org/img/wn/${api.weather.icon}@2x.png">
-        <h4 class="weatherCard__atmosphere">${api.weather.main}: ${api.weather.description}</h4>
-        <h4 class="weatherCard__windSpeed">Wind Speed: ${api.wind_speed/1.609}mph</h4>
-        <h4 class="weatherCard__high">High: ${(api.temp.max-273.15)*(9/5)+32}&#8457;</h4>
-        <h4 class="weatherCard__low">Low: ${(api.temp.min-273.15)*(9/5)+32}&#8457;</h4>
+        <img class="weatherCard__icon" src="http://openweathermap.org/img/wn/${api.weather[0].icon}@2x.png">
+        <h4 class="weatherCard__atmosphere">${api.weather[0].main}: ${api.weather[0].description}</h4>
+        <h4 class="weatherCard__windSpeed">Wind Speed: ${(api.wind_speed/1.609).toFixed(2)}mph</h4>
+        <h4 class="weatherCard__high">High: ${((api.temp.max-273.15)*(9/5)+32).toFixed(1)}&#8457;</h4>
+        <h4 class="weatherCard__low">Low: ${((api.temp.min-273.15)*(9/5)+32).toFixed(1)}&#8457;</h4>
         <h4 class="weatherCard__humidity">Humidity: ${api.humidity}&#37;</h4>
         <h4 class="weatherCard__uvi">UV Index: ${api.uvi}</h3>
     </div>
