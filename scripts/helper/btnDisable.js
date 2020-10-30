fcf73af43a1073dffa388b6e38846159f0e94923
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-30 09:31:59 -0500

    save button disables

--- scripts/helper/btnDisable.js
+++ scripts/helper/btnDisable.js
@@ -1,16 +1,2 @@
 
-const saveBtn = document.querySelector("#saveButton")
-saveBtn.disabled = true
 
-eventHub.addEventListener("change",event => {
-    const parkFilter = document.querySelector("#parksFilter").value
-    const bizarreFilter = document.querySelector("#bizarreFilter").value
-    const eatsFilter = document.querySelector("#eatsFilter").value
-    console.log(parkFilter, bizarreFilter, eatsFilter, 400)
-    if(parkFilter.value === "0" || bizarreFilter === "0" || eatsFilter.value === "0") {
-        saveBtn.disabled = true
-    } else {
-        saveBtn.disabled = false
-    }
-
-})
